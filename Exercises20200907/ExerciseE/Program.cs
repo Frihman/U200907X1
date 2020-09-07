987c1a1fa4553985293cfbd7267b4fa14e09d54f
James Frih <jamesfrih@me.com>
2020-09-07 16:22:01 +0200

    edit7

--- Exercises20200907/ExerciseE/Program.cs
+++ Exercises20200907/ExerciseE/Program.cs
@@ -10,6 +10,31 @@ namespace ExerciseE
     {
         static void Main(string[] args)
         {
+            bool running = true;
+            List<string> words = new List<string>();
+
+            while (running)
+            {
+                Console.WriteLine("Mata in ett ord. Skriv 'AVSLUTA' for att fullborda inmatningen");
+                string input = Console.ReadLine();
+                if (input == "AVSLUTA")
+                {
+                    running = false;
+                    break;
+
+                } else
+                {
+                    words.Add(input);
+                }
+                
+            }
+
+            foreach (string word in words)
+            {
+                Console.Write(word + " ");
+            }
+
+            Console.WriteLine("");
         }
     }
 }
