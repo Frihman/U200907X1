f39577ffee976353570a6366bfe020893c89adc0
James Frih <jamesfrih@me.com>
2020-09-07 11:34:12 +0200

    first commit

---
+++ Exercises20200907/ExerciseA/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mata in en mening");
+            char[] delimiterChars = { ' ', ',', ':' };
+
+            string text = Console.ReadLine();
+
+            string[] words = text.Split(delimiterChars);
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word + " (" + word.Length + ")");
+
+            }
+        }
+    }
+}
