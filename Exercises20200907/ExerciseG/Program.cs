6c0d43c1a8be071d7cef96a29879822b2aa56a9f
James Frih <jamesfrih@me.com>
2020-09-07 19:45:14 +0200

    edit10

---
+++ Exercises20200907/ExerciseG/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDictionary<string, string> leet = new Dictionary<string, string>();
+
+            leet["4"] = "a";
+            leet["8"] = "b";
+            leet["("] = "c";
+        }
+    }
+}
