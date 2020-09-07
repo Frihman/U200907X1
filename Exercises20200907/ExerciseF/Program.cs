2096f68ecbde7c9cb865cb1ddd730a2e3bbfaba2
James Frih <jamesfrih@me.com>
2020-09-07 16:35:33 +0200

    edit9

---
+++ Exercises20200907/ExerciseF/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int number = rand.Next(1, 21);
+            
+            Console.WriteLine("Gissa numret mellan 1 och 21");
+
+            bool running = true;
+            
+            while (running)
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+
+                if (input == number)
+                {
+                    Console.WriteLine("DU GISSADE RÄTT!!");
+                    running = false;
+                }
+                else
+                {
+                    Console.WriteLine("Inte korrekt. Gissa igen");
+                }
+            }
+            
+        }
+    }
+}
