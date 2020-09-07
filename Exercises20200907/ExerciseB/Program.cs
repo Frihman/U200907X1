66ea23631e0d80643eede7fa7b7ecdf973cc8a09
James Frih <jamesfrih@me.com>
2020-09-07 11:35:10 +0200

    edit1

---
+++ Exercises20200907/ExerciseB/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mata in en mening");
+            char[] delimiterChars = { ' ', ',', ':' };
+
+            string text = Console.ReadLine();
+
+            string[] words = text.Split(delimiterChars);
+
+            Array.Sort(words, (x, y) => x.Length.CompareTo(y.Length));
+
+            foreach (var word in words.Reverse())
+            {
+                Console.WriteLine(word + " (" + word.Length + ")");
+                Console.WriteLine("");
+            }
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word + " (" + word.Length + ")");
+
+            }
+        }
+    }
+}
