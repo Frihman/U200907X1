8ace817702abf3d369f479be4cf34f4b64b93bbe
James Frih <jamesfrih@me.com>
2020-09-07 19:56:02 +0200

    edit11

--- Exercises20200907/ExerciseG/Program.cs
+++ Exercises20200907/ExerciseG/Program.cs
@@ -12,9 +12,34 @@ namespace ExerciseG
         {
             IDictionary<string, string> leet = new Dictionary<string, string>();
 
-            leet["4"] = "a";
-            leet["8"] = "b";
-            leet["("] = "c";
+            leet["a"] = "4";
+            leet["b"] = "8";
+            leet["c"] = "(";
+            leet["d"] = "|)";
+            leet["e"] = "3";
+            leet["f"] = "|=";
+            leet["g"] = "9";
+            leet["h"] = "|-|";
+            leet["i"] = "|";
+            leet["j"] = "_|";
+            leet["k"] = "|<";
+            leet["l"] = "1";
+            leet["m"] = "|'|'|";
+            leet["n"] = "N";
+            leet["o"] = "0";
+            leet["p"] = "|D";
+            leet["q"] = "(,)";
+            leet["r"] = "|2";
+            leet["s"] = "5";
+            leet["t"] = "+";
+            leet["u"] = "|_|";
+            leet["v"] = "|/";
+            leet["w"] = "W";
+            leet["x"] = "><";
+            leet["y"] = "j";
+            leet["z"] = "2";
+
+
         }
     }
 }
