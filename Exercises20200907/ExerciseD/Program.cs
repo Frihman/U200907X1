d115b8f25a1181a2f766286f8327ab3f0e0a1bb1
James Frih <jamesfrih@me.com>
2020-09-07 15:55:13 +0200

    edit5

--- Exercises20200907/ExerciseD/Program.cs
+++ Exercises20200907/ExerciseD/Program.cs
@@ -1,35 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ExerciseC
+namespace ExerciseD
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Mata in en mening");
-            char[] delimiterChars = { ' ', ',', ':', '?', '"'};
+            char[] delimiterChars = { ' ', ',', ':', '?', '"' };
 
             string text = Console.ReadLine();
 
-            string[] words = text.Split(delimiterChars);
+            char[] array = text.ToCharArray();
             IDictionary<string, int> wordCount = new Dictionary<string, int>();
 
-            List<string> list = new List<string>(words);
+            List<string> list = new List<string>(array);
 
             Console.WriteLine(words.Length);
 
-            foreach (string word in list.ToList())
+            foreach (string chara in list.ToList())
             {
-                var target = word;
+                var target = chara;
                 int count = 0;
-                
+
                 if (wordCount.ContainsKey(target))
-                { 
+                {
                     count = wordCount[target];
                 }
 
@@ -48,17 +47,13 @@ namespace ExerciseC
 
                 wordCount[target] = count;
 
-               
+
             }
 
-            foreach(KeyValuePair<string, int> entry in wordCount)
+            foreach (KeyValuePair<string, int> entry in wordCount)
             {
                 Console.WriteLine(entry);
             }
-
-            
-
-           
         }
     }
 }
