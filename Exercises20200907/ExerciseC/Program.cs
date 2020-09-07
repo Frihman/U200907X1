cdedaa46a88961d06fbf973cef6bb0f8fc14c81b
James Frih <jamesfrih@me.com>
2020-09-07 11:58:51 +0200

    edit3

--- Exercises20200907/ExerciseC/Program.cs
+++ Exercises20200907/ExerciseC/Program.cs
@@ -18,25 +18,35 @@ namespace ExerciseC
             string[] words = text.Split(delimiterChars);
             IDictionary<string, int> wordCount = new Dictionary<string, int>();
 
-            for (int i = 0; i <= words.Length; i++)
+            for (int i = 0; i < words.Length; i++)
             {
                 bool exists = false;
                 var target = words[i];
-                wordCount[target] = 0;
+                int count = 0;
+                wordCount[target] = count;
 
                 words = words.Where(w => w != words[i]).ToArray();
                 exists = words.Contains(target);
 
+                
                 while (exists)
                 {
                     words = words.Where(w => w != words[i]).ToArray();
                     exists = words.Contains(target);
-                    wordCount[target] += 1;
+                    count += 1;
                 }
 
-                wordCount[target] += 1;
+                count += 1;
+                wordCount[target] = count;
 
             }
+
+            foreach(KeyValuePair<string, int> entry in wordCount)
+            {
+                Console.WriteLine(entry);
+            }
+
+           
         }
     }
 }
